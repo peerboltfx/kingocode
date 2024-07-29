ee6c9958332281f5910b8577e2b2826d476abb1a
peerboltfx <boltpeer@gmail.com>
2024-07-29 12:14:08 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { Routes, Route, useLocation, BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import { AnimatePresence } from "framer-motion";
+import GlobalStyle from "./globalStyles";
+
+//Components
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
+import SoundBar from "./subComponents/SoundBar";
+
+function App() {
+  // const location = useLocation();
+  return (
+    <><Router>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <SoundBar />
+
+        {/* For framer-motion animation on page change! */}
+        {/* Changed prop from exitBefore to mode */}
+        <AnimatePresence mode='wait'>
+          {/* Changed Switch to Routes */}
+
+          <Routes >
+            {/* Changed component to element */}
+
+            <Route path="/" element={<Main />} />
+
+            <Route path="/about" element={<AboutPage />} />
+
+            <Route path="/blog" element={<BlogPage />} />
+
+            <Route path="/work" element={<WorkPage />} />
+
+            <Route path="/skills" element={<MySkillsPage />} />
+            {/* Below is to catch all the other routes and send the user to main component,
+you can add custom 404 component or message instead of Main component*/}
+            <Route path="*" element={<Main />} />
+          </Routes>
+        </AnimatePresence>
+      </ThemeProvider>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/WorkData.jsx
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Dating Site",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://wedlock-pabw.onrender.com",
+        github:"https://github.com/peerboltfx/wedlock"
+    },
+    {
+        id:2,
+        name:"Logistic company",
+        description:"A freight management system for international logistic company",
+        tags:["react","sass"],
+        
+        demo:"#",
+        github:"https://github.com/peerboltfx/freightmaze"
+        
+    },
+    {
+        id:3,
+        name:"Paybit Fx",
+        description:"A dynamic website designed to manage trading analytics",
+        tags:["react","api","sass"],
+        
+        demo:"#",
+        github:"https://github.com/peerboltfx/Paybit-fx"
+    },
+    {
+        id:4,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://react-sidebar.vercel.app/",
+        github:"https://github.com/codebucks27/react-sidebar"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    },
+    {
+        id:6,
+        name:"Responsive Navigation Menu",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    },
+    
+  
+] 
